7c0de43b4b4945fb46d5a98204e06195f6bebade
tialedencan <tialedencan@gmail.com>
2023-02-19 11:38:25 +0100

    Add an instrumentation test

---
+++ TipTime/app/src/androidTest/java/com/example/tiptime/TipUITests.kt
@@ -0,0 +1,53 @@
+package com.example.tiptime
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createEmptyComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.tiptime.ui.theme.TipTimeTheme
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+import java.text.NumberFormat
+
+
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent { 
+            TipTimeTheme {
+                Surface (modifier = Modifier.fillMaxSize()){
+                    TipTimeScreen()
+                }
+
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)").performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found."
+        )
+
+    }
+}
