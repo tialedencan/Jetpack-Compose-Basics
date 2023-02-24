040f4d2cff0780fbf211563f307a39fe99b6f394
tialedencan <tialedencan@gmail.com>
2023-02-24 12:40:57 +0100

    Add unit test for ViewModel - the success path

--- basic-android-kotlin-compose-training-unscramble-starter/app/src/test/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
+++ basic-android-kotlin-compose-training-unscramble-starter/app/src/test/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
@@ -1,4 +1,29 @@
 package com.example.android.unscramble.ui.test
 
+import com.example.android.unscramble.data.SCORE_INCREASE
+import com.example.android.unscramble.data.getUnscrambledWord
+import com.example.android.unscramble.ui.GameViewModel
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import org.junit.Test
+
 class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset()  {
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        // Assert that checkUserGuess() method updates isGuessedWordWrong is updated correctly.
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        // Assert that score is updated correctly.
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+    }
+    companion object {
+        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
 }
